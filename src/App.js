fdefcbc4c721bc947ecae26dd5d79e156cb1266a
ZeroJimmy100 <61480298+ZeroJimmy100@users.noreply.github.com>
2020-04-12 00:07:22 -0700

    Hangman Game

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import Hangman from './components/Hangman';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Hangman/>
+    </div>
+  );
+}
+
+export default App;
